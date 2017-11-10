c980436a523e44efb33cc16b359f6fa912e997cf
valentin4ahif <32124205+valentin4ahif@users.noreply.github.com>
2017-11-10 13:30:34 +0100

    hausuebung abgabe

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+/*$("#submitButton").prop('disabled', true);*/
+$(':input[type="submit"]').prop('disabled', true);
+$("#emailMandatory").hide();
+$("#otherMediaChannel").hide();
+$(".form-group").change(function () {
+    let error = false;
+    if ($("#firstName").val() != "") {
+        $("#firstNameMandatory").hide();
+    }
+    else {
+        $("#firstNameMandatory").show();
+        error = true;
+    }
+    if ($("#lastName").val() != "") {
+        $("#lastNameMandatory").hide();
+    }
+    else {
+        $("#lastNameMandatory").show();
+        error = true;
+    }
+    if ($("#newsletter").is(':checked')) {
+        if ($("#email").val() != "") {
+            $("#emailMandatory").hide();
+        }
+        else {
+            $("#emailMandatory").show();
+            error = true;
+        }
+    }
+    else {
+        $("#emailMandatory").hide();
+    }
+    if ($("#mediaChannelSelect").val() != "Other") {
+        $("#otherMediaChannel").hide();
+    }
+    else {
+        $("#otherMediaChannel").show();
+        if ($("#otherMediaChannel").val() == "")
+            error = true;
+    }
+    if (error === false) {
+        $(':input[type="submit"]').prop('disabled', false);
+    }
+    else {
+        $(':input[type="submit"]').prop('disabled', true);
+    }
+});
